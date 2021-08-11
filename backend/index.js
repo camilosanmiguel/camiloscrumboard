159fa159aa9ca07d96c31ebb64137659628608fe
Camilo Sanmiguel <camilo.sanmiguel@gmail.com>
2021-08-11 16:03:16 -0500

    replica clase 4

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,15 @@ const cors = require("cors");
 const { dbConnection } = require("./db/db");
 require("dotenv").config();
 
+const Role = require("./routes/role");
+const User = require("./routes/user");
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/role",Role);
+app.use("/api/user",User);
 
 app.listen(process.env.PORT, () => console.log("backen server runnin ok, on port: ",process.env.PORT));
 
